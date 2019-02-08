5c97dfab34e5139612a5244b3c50a2658a39f0da
Davoleo <davoleo2001@gmail.com>
2019-02-08 22:08:23 +0100

    Linked MainActivity to GG Activity

--- app/src/main/java/net/davoleo/java_android/MainActivity.java
+++ app/src/main/java/net/davoleo/java_android/MainActivity.java
@@ -1,7 +1,9 @@
 package net.davoleo.java_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGeoGenius(View view)
+    {
+        Intent intent = new Intent(this, GeoGenius.class);
+        startActivity(intent);
     }
 }
