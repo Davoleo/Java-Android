8d8b9f4fb3d75601617920a9ca2b787b3b7c2d87
Davoleo <davoleo2001@gmail.com>
2019-02-19 17:13:00 +0100

    Menu Items handler

--- app/src/main/java/net/davoleo/java_android/GeoGeniusHome.java
+++ app/src/main/java/net/davoleo/java_android/GeoGeniusHome.java
@@ -2,20 +2,59 @@ package net.davoleo.java_android;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
+import net.davoleo.java_android.sections.Capitals;
 
-public class GeoGeniusHome extends Activity {
+public class GeoGeniusHome extends AppCompatActivity {
+
+    private String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_genius_home);
+        setTitle("Geo Genius");
 
         Intent i = getIntent();
-        String user = i.getStringExtra("user");
+        user = i.getStringExtra("user");
         Toast.makeText(getApplicationContext(), "Welcome back " + user + " :)", Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.geo_genius_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_capitals:
+                Intent intent = new Intent(this, Capitals.class);
+                Bundle b = new Bundle();
+                b.putString("user", user);
+                intent.putExtras(b);
+                startActivity(intent);
+                break;
+            case R.id.action_rivers:
+                break;
+            case R.id.action_lakes:
+                break;
+            case R.id.action_monuments:
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
 }
---
+++ app/src/main/java/net/davoleo/java_android/sections/Capitals.java
@@ -0,0 +1,16 @@
+package net.davoleo.java_android.sections;
+
+import android.os.Bundle;
+import android.app.Activity;
+import net.davoleo.java_android.R;
+
+public class Capitals extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capitals);
+    }
+
+}
