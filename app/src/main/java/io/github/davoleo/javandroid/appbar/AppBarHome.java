d87fe3eac08fcb03f1836fabb40f086b8da70fe1
Davoleo <davoleo2001@gmail.com>
2020-05-11 15:29:09 +0200

    actionBar buttons

--- app/src/main/java/io/github/davoleo/javandroid/appbar/AppBarHome.java
+++ app/src/main/java/io/github/davoleo/javandroid/appbar/AppBarHome.java
@@ -1,5 +1,10 @@
 package io.github.davoleo.javandroid.appbar;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
@@ -16,5 +21,30 @@ public class AppBarHome extends AppCompatActivity {
 
         toolbar = findViewById(R.id.customToolbar);
         setSupportActionBar(toolbar);
+
+        //Add an home button to the support Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.custom_app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                Toast.makeText(this, "Sharing in progress...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Opening App Settings...", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
